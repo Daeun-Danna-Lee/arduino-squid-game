2b0a17f7c175d7fe3fd035b170b80cf3cb41fb1e
이다은 Danna Lee <71601985+Daeun-Danna-Lee@users.noreply.github.com>
2021-12-06 21:12:18 +0900

    [feat] 블루투스 모듈 연결

--- player-module/player-module.ino
+++ player-module/player-module.ino
@@ -5,79 +5,91 @@
  100 ~ 180 : 역방향으로 (180-빠르게 / 100-느리게)
 */
 #include <Servo.h> // 서보모터 라이브러리 사용
+#include <SoftwareSerial.h> // 블루투스 시리얼 통신을 위한 라이브러리
+
+// TX, RX 핀 지정
+#define BT_TXD 2
+#define BT_RXD 3
+
+// 블루투스 모듈 선언
+SoftwareSerial bluetooth(BT_TXD, BT_RXD);
+
+// 서보 모터 선언
 Servo servo_9;
 Servo servo_11;
 
 int command = 0;
-void setup()
-{
+int prev_command = 0;
 
+void setup() {
   Serial.begin(9600);
-  
+  bluetooth.begin(9600);
+  Serial.println("Bluetooth Ready");
   servo_9.attach(9);  // 서보모터 9번핀에 연결
   servo_11.attach(11); // 서보모터 11번핀에 연결
- 
 }
 
-void loop()
-{   
-  if (Serial.available()) {
-    command = Serial.read(); // 1
-
+void loop() {
+  
+  // 블루투스로 값이 들어오면
+  if (bluetooth.available()) {
+    command = bluetooth.read(); // 1
+    
     switch (command) {
       case 49: // 1
         moveForward();
-        Serial.println("forward");
+        //Serial.println("forward");
         break;
       case 50: // 2
         moveBackward();
-        Serial.println("backward");
+        //Serial.println("backward");
         break;
       case 51: // 3
         turnRight();
-        Serial.println("right");
-        break;
-      case 52: // 4
-        turnLeft();
-        Serial.println("left");
+        //Serial.println("right");
         break;
       case 53: // 5
         stopMoving();
-        Serial.println("stop");
+        //Serial.println("stop");
         break;
+      case 52: // 4
+        turnLeft();
+        //Serial.println("left");
+        break;
+      
       default:
         break;
     }
+    prev_command = command;
   }
-  
 }
 
 void moveForward() {
   // 전진
-  servo_9.write(80);  // 정방향으로 가장 느리게 회전
-  servo_11.write(110); // 역방향으로 진장 느리게 회전 
+  servo_9.write(70);
+  servo_11.write(110);
 }
 
 void moveBackward() {
   // 후진 
-  servo_9.write(110);  // 역방향으로 가장 느리게 회전
-  servo_11.write(80); // 정방향으로 진장 느리게 회전 
+  servo_9.write(110);
+  servo_11.write(70);
 }
 
 void turnRight() {
   // 우회전
-  servo_9.write(90);  // 정지 
-  servo_11.write(110); // 역방향으로 진장 느리게 회전
+  servo_9.write(120);
+  servo_11.write(120);
 }
 
 void turnLeft() {
   // 좌회전 
-  servo_9.write(80);  // 정방향으로 가장 느리게 회전
-  servo_11.write(90); // 정지 
+  servo_9.write(60);
+  servo_11.write(60);
 }
 
 void stopMoving() {
   // 정지 
-  servo_9.write(90);  // 정지 
-  servo_11.write(90); // 정지 
+  servo_9.write(90);
+  servo_11.write(90);
 }
